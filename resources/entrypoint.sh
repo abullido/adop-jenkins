bd4cb834719effb5d548eb9f479c21efb54b38bf
abullido <a.bullidogonzalez@accenture.com>
2016-03-15 13:19:37 +0100

    Update entrypoint.sh

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -8,5 +8,5 @@ password=$GERRIT_JENKINS_PASSWORD
 nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
 echo "start JENKINS"
 
-chown -R 1000:1000 /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home
 su jenkins -c /usr/local/bin/jenkins.sh
